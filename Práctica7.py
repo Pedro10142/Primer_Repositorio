641d95a6311a4ae7b5dc010b4b1b09589d6dd653
Pedro Medina <angelmedinapedro@gmail.com>
2025-03-27 08:38:25 -0600

    add.Práctica7.py

---
+++ Práctica7.py
@@ -0,0 +1,21 @@
+from numpy import array
+import numpy as np
+
+lista = (["hola", 24, True, "si", 24])
+
+listaNp = np.array([5,6,7,8,9])
+
+tupla = tuple(lista)
+tupla2 = tuple(listaNp)
+
+conjunto = set(lista)
+conjunto2 = set(listaNp)
+
+D ={
+        "nombre": "Pedro",
+         "edad": 30,
+         "ciudad": "Zacatecas",
+         "cash" : 0,
+         "vivo": False,
+}
+
