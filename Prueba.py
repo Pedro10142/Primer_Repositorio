5d303ccb9f834660c926c0e29c68b14e4e4be1e3
Pedro Medina <angelmedinapedro@gmail.com>
2025-04-29 09:02:54 -0600

    ADD.Prueba.py

--- Prueba.py
+++ Prueba.py
@@ -2,13 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/hola')
-def hola_mundo():
-    return "Hola mundo" 
-
 @app.route('/')
-def hola_mundo2():
-    return "Hola mundo 2" 
+def hola_mundo():
+    return "Pepino" 
+
+# @app.route('/hola') # al cambiar la extension desde aqui se puede acceder a otra ventana distinta (/hola)
+# def hola_mundo():
+#   return "Hola mundo 2" 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host = '0.0.0.0', port=5000)
+    
